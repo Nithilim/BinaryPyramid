aa7d5687a109e3b4a2d0424bf2645da99d7fe6ca
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-09-02 01:37:43 +0300

    add whole code.

---
+++ BinaryPyramid/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryPyramid
+{
+    public class Node
+    {
+        public int Data { get; }
+
+        public int Layer { get; }
+
+        public Node LeftChild { get; private set; }
+
+        public Node RightChild { get; private set; }
+
+        public bool HasChildren { get; private set; }
+
+        public Node(int data, int layer)
+        {
+            Data = data;
+            Layer = layer;
+            HasChildren = false;
+        }
+
+        public Node(int data, Node leftChild, Node rightChild)
+        {
+            Data = data;
+            LeftChild = leftChild;
+            RightChild = rightChild;
+            HasChildren = true;
+        }
+
+        public void SetSymmetricalChildren(Node leftData, Node rightData)
+        {
+            HasChildren = true;
+            LeftChild = leftData;
+            RightChild = rightData;
+        }
+    }
+}
---
+++ BinaryPyramid/SymmetricalBinaryTree.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryPyramid
+{
+    public class SymmetricalBinaryTree : BinaryTree
+    {
+        public int TotalLevels { get; private set; }
+
+        public SymmetricalBinaryTree(Node root) : base(root)
+        {
+        }
+
+        public SymmetricalBinaryTree() : base(null)
+        {
+        }
+
+        public BinaryTree BuildTreeFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                var fileInfo = new FileInfo(path);
+                int maxLevel = GetLevelCountFromFile(fileInfo);
+                Node rootNode = default;
+                var lastLayer = new List<Node>();
+                if (maxLevel > 0)
+                {
+                    for (int currentLevel = 1; currentLevel <= maxLevel; currentLevel++)
+                    {
+                        var currentLayer = GetLevelContent(currentLevel, fileInfo).Select(num => new Node(num, currentLevel)).ToList();
+                        if (currentLevel == 1)
+                        {
+                            rootNode = currentLayer[0];
+                        }
+
+                        else
+                        {
+                            foreach (Node node in lastLayer)
+                            {
+                                int pos = lastLayer.IndexOf(node);
+                                node.SetSymmetricalChildren(currentLayer[pos], currentLayer[pos + 1]);
+                            }
+                        }
+
+                        lastLayer = currentLayer;
+                    }
+                }
+
+                return new BinaryTree(rootNode, maxLevel);
+            }
+
+            return null;
+        }
+
+        private int GetLevelCountFromFile(FileInfo info)
+        {
+            int level = 0;
+            using(var reader = new StreamReader(info.FullName))
+            {
+                while (reader.ReadLine() != null)
+                    level++;
+
+                return level;
+            }
+        }
+
+        private List<int> GetLevelContent(int level, FileInfo info)
+        {
+            string line = "";
+            using(var reader = new StreamReader(info.FullName))
+            {
+                for(int lineNumber = 1; lineNumber <= level; lineNumber++)
+                {
+                    if (lineNumber == level)
+                    {
+                        line = reader.ReadLine();
+                    }
+                    else
+                        reader.ReadLine();
+                }
+            }
+            
+            var stringNumbers = line.Split(" ");
+            var result = new List<int>();
+            foreach(string num in stringNumbers)
+            {
+                int number;
+                bool parseResult = int.TryParse(num, out number);
+                if (parseResult)
+                    result.Add(number);
+            }
+
+            return result;
+        }
+    }
+}
